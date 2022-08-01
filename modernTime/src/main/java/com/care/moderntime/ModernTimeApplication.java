687f4309607d632c6566666b6be5daddd86bfe29
test <dlskrud6141@naver.com>
2022-08-01 20:39:48 +0900

    jjin first push test!

---
+++ modernTime/src/main/java/com/care/moderntime/ModernTimeApplication.java
@@ -0,0 +1,13 @@
+package com.care.moderntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernTimeApplication.class, args);
+	}
+
+}
---
+++ modernTime/src/test/java/com/care/moderntime/ModernTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.care.moderntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
