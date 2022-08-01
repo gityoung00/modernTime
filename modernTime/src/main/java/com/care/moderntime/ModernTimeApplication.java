36b5e061f9b2c637245808309494ac5cfb368db2
dihar <dihar@DESKTOP-R42I0S6>
2022-08-01 21:41:45 +0900

    kiup

--- modernTime/src/main/java/com/care/moderntime/ModernTimeApplication.java
+++ modernTime/src/main/java/com/care/moderntime/ModernTimeApplication.java
@@ -2,8 +2,9 @@ package com.care.moderntime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ModernTimeApplication {
 
 	public static void main(String[] args) {
