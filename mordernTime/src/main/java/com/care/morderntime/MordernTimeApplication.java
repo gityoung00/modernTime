4713c22b10f152b55884739300c4f38642e6b4c1
test <dlskrud6141@naver.com>
2022-08-01 19:53:58 +0900

    first push test

---
+++ mordernTime/src/main/java/com/care/morderntime/MordernTimeApplication.java
@@ -0,0 +1,13 @@
+package com.care.morderntime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MordernTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MordernTimeApplication.class, args);
+	}
+
+}
---
+++ mordernTime/src/test/java/com/care/morderntime/MordernTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.care.morderntime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MordernTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
